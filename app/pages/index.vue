cb066c7ed054ae5a57b2373eeac779ef94d316b1
hattinghj <98312411+hattinghjohan@users.noreply.github.com>
2022-01-25 08:48:25 +0400

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -37,7 +37,7 @@
         <img
           alt="Hero"
           class="rounded shadow-xl"
-          src="https://source.unsplash.com/random/720x400"
+          src="https://picsum.photos/720/400?blur=1"
         />
       </div>
     </div>
